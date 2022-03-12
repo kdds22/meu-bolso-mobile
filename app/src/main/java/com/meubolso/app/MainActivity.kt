eb542f2e3ccd430693ed0ec719313a8e29718b9e
Klaus Dellano <klausdell@hotmail.com>
2022-03-12 20:10:35 -0300

    Configurando Gradle para comportar outros fragments

--- app/src/main/java/com/meubolso/app/MainActivity.kt
+++ app/src/main/java/com/meubolso/app/MainActivity.kt
@@ -2,9 +2,6 @@ package com.meubolso.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import com.meubolso.app.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
 
