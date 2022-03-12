8ec6122ca86139e12df4177a56802ae8dd80a923
Klaus Dellano <klausdell@hotmail.com>
2022-03-12 20:11:41 -0300

    Sensibilizando injeção de dependencia

--- app/src/main/java/com/meubolso/app/ui/di/UiModule.kt
+++ app/src/main/java/com/meubolso/app/ui/di/UiModule.kt
@@ -1,6 +1,7 @@
 package com.meubolso.app.ui.di
 
-import com.meubolso.app.ui.cadastro.LoginCadastroViewModel
+import com.meubolso.app.ui.cadastro.CadastroViewModel
+import com.meubolso.app.ui.home.HomeViewModel
 import com.meubolso.app.ui.login.LoginViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,12 +9,17 @@ import org.koin.dsl.module
 
 val viewModelApp = module {
     viewModel{
-        LoginViewModel(
+        HomeViewModel(
             androidApplication()
         )
     }
-    viewModel{
-        LoginCadastroViewModel(
+    viewModel {
+        CadastroViewModel(
+            androidApplication()
+        )
+    }
+    viewModel {
+        LoginViewModel(
             androidApplication()
         )
     }
